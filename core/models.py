2a513b3eeefea5e0d97c5e33c58d95c76702c1ba
mobeen777 <mobeen.mobeen777@gmail.com>
2022-09-04 13:41:47 +0500

    Adding Sign Up Functionality

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 # Create your models here.
 
 class Profile(models.Model):
--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import *
 from django.http import HttpResponse
 
 
@@ -6,3 +9,37 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        print(username)
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+
+        if password == password2:
+
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email is already taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'User is already Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                # Log user in and redirect to settings page
+
+                # create a profile objec for new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
