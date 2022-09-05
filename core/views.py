000fe98bd362f6767cb43aeecf6fcbe8e7421033
mobeen777 <mobeen.mobeen777@gmail.com>
2022-09-05 23:47:45 +0500

    Refactor The Code A little

--- core/views.py
+++ core/views.py
@@ -22,13 +22,14 @@ def signup(request):
         password2 = request.POST.get("password2")
 
         if password == password2:
-
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email is already taken')
                 return redirect('signup')
+
             elif User.objects.filter(username=username).exists():
                 messages.info(request, 'User is already Taken')
                 return redirect('signup')
+
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
@@ -36,13 +37,11 @@ def signup(request):
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-
                 return redirect('signin')
 
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
-
     else:
         return render(request, 'signup.html')
 
@@ -51,17 +50,14 @@ def signin(request):
     if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
-
         user = auth.authenticate(username=username, password=password)
 
         if user is not None:
             auth.login(request, user)
             return redirect('/')
-
         else:
             messages.info(request, 'Credentials Invalid')
             return redirect('signin')
-
     else:
         return render(request, 'signin.html')
 
