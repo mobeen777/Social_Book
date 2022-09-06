6d744d31b446997a87f3e9b9cfffce7dbadf0c85
mobeen777 <mobeen.mobeen777@gmail.com>
2022-09-06 19:53:40 +0500

    Adding Accounts Settings

--- core/views.py
+++ core/views.py
@@ -34,10 +34,13 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signin')
+                return redirect('settings')
 
         else:
             messages.info(request, 'Password Not Matching')
@@ -66,3 +69,33 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') is None:
+            image = user_profile.profileimg
+            bio = request.POST.get('bio')
+            location = request.POST.get('location')
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') is not None:
+            bio = request.POST.get('bio')
+            location = request.POST.get('location')
+            image = request.FILES.get('image')
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
