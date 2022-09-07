1e1e58f234b26f1ff2a9a0a2c0aaec8dd22bb9c2
mobeen777 <mobeen.mobeen777@gmail.com>
2022-09-07 23:40:02 +0500

    Refactoring

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import *
-from django.http import HttpResponse
+
 
 
 # Create your views here.
